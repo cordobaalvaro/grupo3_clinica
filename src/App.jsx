0ed88aeed6dde915b28a799664a4631320c4551f
NahuelJimenezDev <102840530+NahuelJimenezdev@users.noreply.github.com>
2025-03-19 23:09:52 -0300

    Se realiza la creacion de los registros tanto de usuario como de admin, se redireccion de registro a login y de login a su respectiva vista

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import "./App.css";
 import HomePage from "./page/HomePage";
 import SpecialistDetailsPage from "./page/SpecialistDetailsPage";
 import SpecialistPage from "./page/SpecialistPage";
+import LoginForm from "./components/forms/LoginForm";
+import AdminPage from "./page/AdminPage";
+import UserPage from "./page/UserPage";
 
 function App() {
   return (
@@ -13,7 +16,10 @@ function App() {
       <NavbarPage idPage='administrador' />
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='/registerpage' element={<RegisterPage />} />
+        <Route path='/inicio-de-sesion' element={<LoginForm />} />
+        <Route path='/pagina-de-usuario' element={<UserPage />} />
+        <Route path='/pagina-de-administrador' element={<AdminPage />} />
+        <Route path='/pagina-registro' element={<RegisterPage />} />
         <Route path='/detalle-especialista/:nombre' element={<SpecialistDetailsPage />} />
         <Route path='/pagina-especialista' element={<SpecialistPage />} />
       </Routes>
--- src/components/forms/LoginForm.jsx
+++ src/components/forms/LoginForm.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import { Card, Button, Form, Col, Row } from "react-bootstrap";
+import { useRegisterForm } from "../../hooks/useRegisterForm";
 
 const LoginForm = () => {
+  const {
+    registerUser,
+    errores,
+    handleChangeRegisterForm,
+    handleChangeLoginForm
+  } = useRegisterForm();
   return (
-    <div>LoginForm</div>
-  )
-}
+    <Row className="d-flex justify-content-center">
+      <Col sm={10} md={4} xl={4} className="p-3">
+        <Card className="p-3 mt-3">
+          <Form>
+            <Form.Group className="mb-3" controlId="formGroupEmail">
+              <Form.Label>Nombre de Usuario</Form.Label>
+              <Form.Control type="text" placeholder="Nombre de usuario" name="nameUser" value={registerUser.nameUser} onChange={handleChangeRegisterForm} isInvalid={errores.nameUser} />
+              {
+                <p className="text-danger">{errores.nameUser}</p>
+              }
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formGroupPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" name="passwordUser" value={registerUser.passwordUser} onChange={handleChangeRegisterForm} isInvalid={errores.passwordUser} />
+              {
+                <p className="text-danger">{errores.passwordUser}</p>
+              }
+            </Form.Group>
+          </Form>
+          <Button
+            variant="primary"
+            type="submit"
+            onClick={handleChangeLoginForm}
+            className="w-100"
+          >
+            Iniciar Sesión
+          </Button>
+        </Card>
+      </Col>
+    </Row>
+  );
+};
 
 export default LoginForm
