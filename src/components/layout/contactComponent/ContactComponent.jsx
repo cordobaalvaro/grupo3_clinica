f04dd6c5c2c28765bf09a54bc645a2b5d87b8e49
Alvaro Cordoba <offaim123@gmail.com>
2025-03-24 20:25:04 -0300

    se cambia algunos diseños del formulario de contact page, cambiandole el responsive tambien.

--- src/components/layout/contactComponent/ContactComponent.jsx
+++ src/components/layout/contactComponent/ContactComponent.jsx
@@ -8,11 +8,11 @@ const ContactPage = () => {
     <>
       <Container className='mt-5'>
         <Row>
-          <Col md='6'>
+          <Col md='6' lg='5' xl='6' className='mt-5 d-none d-lg-block'>
             <Map />
           </Col>
 
-          <Col md='6'>
+          <Col md={12} lg={6}>
             <ContactForm />
           </Col>
         </Row>
